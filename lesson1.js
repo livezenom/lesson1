5f0155a887b5bfb08e01c38f6f126a240a6b7481
Evgeniy Samokhin <zenom@Evgeniys-MacBook-Pro.local>
2018-03-06 00:28:08 +0300

    polyfill added

--- lesson1.js
+++ lesson1.js
@@ -1,7 +1,5 @@
 function sum(a) {
-
     var allsum = a;
-  
     function foo(b) {
         allsum += b;
       return foo;
@@ -11,5 +9,30 @@ function sum(a) {
     };
   return foo;
   }
-
   console.log (sum(1)(2)(3)(4)(10)(-1));
+
+
+  //polifill Object.create
+  if (typeof Object.create != 'function') {
+    Object.create = (function() {
+      function Temp() {}        
+      var hasOwn = Object.prototype.hasOwnProperty;
+      return function (O) {
+        if (typeof O != 'object') {
+          throw TypeError('Object prototype may only be an Object or null');
+        }
+        Temp.prototype = O;
+        var obj = new Temp();
+        Temp.prototype = null; 
+        if (arguments.length > 1) {
+          var Properties = Object(arguments[1]);
+          for (var prop in Properties) {
+            if (hasOwn.call(Properties, prop)) {
+              obj[prop] = Properties[prop];
+            }
+          }
+        }
+        return obj;
+      };
+    })();
+  }
